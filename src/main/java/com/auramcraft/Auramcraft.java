37132b2a55dc0f25576ccc77a1700ca7c5fbf713
Xydium <prodriver54@gmail.com>
2014-06-09 20:07:27 -0400

    Test commit
    FFS I hope this works

--- src/main/java/com/auramcraft/Auramcraft.java
+++ src/main/java/com/auramcraft/Auramcraft.java
@@ -1,5 +1,7 @@
 package com.auramcraft;
 
+//Xydium here, making sure Git is behaving. Ignore this comment
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
