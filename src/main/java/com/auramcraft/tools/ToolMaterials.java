451b26cc244013cf2c9b141b170dbb30b585f356
Xydium <prodriver54@gmail.com>
2014-06-12 15:50:48 -0400

    Fixed Sacrificial Blade
    Blade still dealt 24 damage instead of the intended 4.

--- src/main/java/com/auramcraft/tools/ToolMaterials.java
+++ src/main/java/com/auramcraft/tools/ToolMaterials.java
@@ -4,5 +4,5 @@ import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ToolMaterials {
-	public static ToolMaterial sacrificialMaterial = EnumHelper.addToolMaterial("SacrificialMaterial", 0, 10, 0, 20, 0);
+	public static ToolMaterial sacrificialMaterial = EnumHelper.addToolMaterial("SacrificialMaterial", 0, 10, 0, 0, 0);
 }
