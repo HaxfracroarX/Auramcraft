0a8c75d4709074e6b256db77d428754786148f88
Mcat12 <newtoncat12@yahoo.com>
2015-06-01 18:26:11 -0400

    Changed function of Aura symbols in Infusion Table Used to show what auras were in the AuraItem, now shows what auras are needed to craft Soon to be adjusted to show how many auras needed to craft

--- src/main/java/com/auramcraft/client/gui/inventory/GuiInfusionTable.java
+++ src/main/java/com/auramcraft/client/gui/inventory/GuiInfusionTable.java
@@ -1,22 +1,24 @@
 package com.auramcraft.client.gui.inventory;
 
-import org.lwjgl.opengl.GL11;
 import com.auramcraft.api.Auras;
 import com.auramcraft.inventory.ContainerInfusionTable;
-import com.auramcraft.item.AuraItem;
+import com.auramcraft.item.crafting.AuramcraftCraftingManager;
+import com.auramcraft.item.crafting.IInfusionRecipe;
 import com.auramcraft.reference.Textures;
 import com.auramcraft.tileentity.TileInfusionTable;
-import com.auramcraft.util.LogHelper;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.world.World;
+import org.lwjgl.opengl.GL11;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @SuppressWarnings("unchecked")
 public class GuiInfusionTable extends GuiContainer {
 	private final TileInfusionTable tileInfusionTable;
 	private final int[] slotX = new int[] {
-			17, 51, 85, 117, 150
+			17, 50, 83, 116, 149
 	};
 	private final Auras[] auras = Auras.values();
 	
@@ -28,54 +30,38 @@ public class GuiInfusionTable extends GuiContainer {
 	}
 	
 	@Override
-	public void initGui() {
-		super.initGui();
-		int x = (width - xSize) / 2;
-	    int y = (height - ySize) / 2;
-
-		int slotY = 60;
-		for(int i = 0; i < 5; i++)
-			buttonList.add(new AuraButton(i, x+slotX[i], y+ slotY, auras[i]));
-	}
-	
-	@Override
 	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
+		// Draw Background
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE);
-		int x = (width - xSize) / 2;
-	    int y = (height - ySize) / 2;
-	    drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
-	    
-	    mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE_AURAS);
-	    for(int i = 0; i < 5; i++) {
-	    	if(tileInfusionTable.getStackInSlot(9) != null) {
-				((GuiButton) buttonList.get(i)).visible = AuraItem.getAuraContainer(tileInfusionTable.getStackInSlot(9)).containsAura(auras[i]);
-	    	}
-	    	else {
-	    		if(((GuiButton) buttonList.get(i)).visible)
-	    			((GuiButton) buttonList.get(i)).visible = false;
-	    	}
-	    }
-	}
-	
-	@Override
-	protected void actionPerformed(GuiButton button) {
-		switch(button.id) {
-			case 0:
-				LogHelper.info("Button 1");
+	    drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+		
+		// Check if there is an item in the output
+		if(tileInfusionTable.getStackInSlot(10) == null)
+			return;
+		
+		// Get infusion recipes
+		ArrayList<IInfusionRecipe> recipes = new ArrayList<IInfusionRecipe>((Collection<? extends IInfusionRecipe>) AuramcraftCraftingManager.getInstance().getRecipeList());
+		
+		// Get the aura needed by the recipe
+		ArrayList recipeAuras = new ArrayList();
+		
+		// Check for matching recipe
+		for(IInfusionRecipe recipe : recipes) {
+			if(recipe.getRecipeOutput().getItem().equals(tileInfusionTable.getStackInSlot(10).getItem())) {
+				recipeAuras = new ArrayList(recipe.getRecipeAuras());
 				break;
-			case 1:
-				LogHelper.info("Button 2");
-				break;
-			case 2:
-				LogHelper.info("Button 3");
-				break;
-			case 3:
-				LogHelper.info("Button 4");
-				break;
-			case 4:
-				LogHelper.info("Button 5");
-				break;
+			}
+		}
+		
+		// Bind Aura textures
+		mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE_AURAS);
+		
+		// Draw Aura Buttons
+		GL11.glColor4f(1f, 1f, 1f, 1f);
+		for(int i = 0; i < 5; i++) {
+			if(recipeAuras.contains(Auras.values()[i]))
+				drawTexturedModalRect(guiLeft + slotX[i], guiTop + 60, ((auras[i].getTier() - 1) * 32), (auras[i].getId() * 32), 32, 32);
 		}
 	}
 }
