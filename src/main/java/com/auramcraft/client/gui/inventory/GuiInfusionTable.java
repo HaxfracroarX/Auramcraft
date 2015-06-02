44baaf80db30c9a2a9fe87ddf5a59f63d297200e
Mcat12 <newtoncat12@yahoo.com>
2015-06-02 10:30:59 -0400

    Adjusted Infusion Table - Changed old Aura buttons to show how much aura is needed for the recipe - Got rid of aura icon display

--- src/main/java/com/auramcraft/client/gui/inventory/GuiInfusionTable.java
+++ src/main/java/com/auramcraft/client/gui/inventory/GuiInfusionTable.java
@@ -29,13 +29,12 @@ public class GuiInfusionTable extends GuiContainer {
 		ySize = 179;
 	}
 	
+	/**
+	 * @param mx Mouse x
+	 * @param my Mouse y
+	 */
 	@Override
-	protected void drawGuiContainerBackgroundLayer(float var1, int var2, int var3) {
-		// Draw Background
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE);
-	    drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-		
+	protected void drawGuiContainerForegroundLayer(int mx, int my) {
 		// Check if there is an item in the output
 		if(tileInfusionTable.getStackInSlot(10) == null)
 			return;
@@ -54,14 +53,30 @@ public class GuiInfusionTable extends GuiContainer {
 			}
 		}
 		
-		// Bind Aura textures
-		mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE_AURAS);
+		// Color of text
+		int textColor = 0x996633;
 		
-		// Draw Aura Buttons
-		GL11.glColor4f(1f, 1f, 1f, 1f);
 		for(int i = 0; i < 5; i++) {
-			if(recipeAuras.contains(Auras.values()[i]))
-				drawTexturedModalRect(guiLeft + slotX[i], guiTop + 60, ((auras[i].getTier() - 1) * 32), (auras[i].getId() * 32), 32, 32);
+			if(recipeAuras.contains(auras[i])) {
+				String name = auras[i].toString();
+				String amount = "x" + recipeAuras.get(recipeAuras.indexOf(auras[i]) + 1);
+				
+				fontRendererObj.drawString(name, slotX[i] + (32 - fontRendererObj.getStringWidth(name))/2, 60 + (32 - fontRendererObj.FONT_HEIGHT)/2 - fontRendererObj.FONT_HEIGHT/2, textColor);
+				fontRendererObj.drawString(amount, slotX[i] + (32 - fontRendererObj.getStringWidth(amount))/2, 60 + (32 - fontRendererObj.FONT_HEIGHT)/2 + fontRendererObj.FONT_HEIGHT/2, textColor);
+			}
 		}
+	}
+	
+	/**
+	 * @param renderPartialTicks Partial ticks from last render
+	 * @param mx Mouse x
+	 * @param my Mouse y
+	 */
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float renderPartialTicks, int mx, int my) {
+		// Draw Background
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		mc.getTextureManager().bindTexture(Textures.GUI.GUI_INFUSION_TABLE);
+	    drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
 	}
 }
